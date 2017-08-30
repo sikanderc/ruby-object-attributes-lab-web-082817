3fbc0b08a3a21aaf9ddd97b8b0bd3eef7db0bee7
github username <github email address>
2017-08-30 19:18:04 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+require "pry"
+
+class Dog
+
+  def name
+    @name
+  end
+
+  def name=(dog_name)
+    @name=dog_name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(dog_type)
+    @breed=dog_type
+  end
+
+end
